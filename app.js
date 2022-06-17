bc41c1f3f22878cc11b0e1ea0fbfc04b84fb43b8
Ashish Biswas <ashishbiswasg@gmail.com>
2022-06-17 14:46:33 +0530

    This is the version-2.0 this is implemented using NOdeJS, Express, MongoDB, Mongoose

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const app=express();
 
+//Mongoose packege:
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
 
-var items=["Buy Food", "Cook Food", "Eat Foot"];
-var workItems=[];
+// Features of v-1.0 i.e array
+// var items=["Buy Food", "Cook Food", "Eat Foot"];
+// var workItems=[];
+
+// in v-2.0 we will use Database MongoDB and Mongoose to store data
+
+const iteamSchema=new mongoose.Schema({
+    name:String
+});
+const Iteam=mongoose.model("toDoList",iteamSchema);
+const iteam1=new Iteam({
+    name:"This is iteam 1"
+});
+const iteam2=new Iteam({
+    name:"This is iteam 2"
+});
+const iteam3=new Iteam({
+    name:"This is iteam 3"
+});
+
+// successfully model objects are created
 
 var today=new Date();
 var day="";
@@ -24,40 +47,72 @@ for(let i=0;i<7;i++){
 }
 
 app.get("/",function(req,res){
-    // res.send("Welcome to todolist app")
+  
+// added into database successfully
+    Iteam.find({},function(err,foundIteam){
+        if(foundIteam.length===0){
+            Iteam.insertMany([iteam1,iteam2,iteam3], function(err){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("Successfully added to database");
+                }
+            });
+            res.redirect("/");
+        }
+        else{
+            res.render("list",{listName:day, iteamArray:foundIteam, year:fullyear});
+        }
+// added into database successfully
+    });
     
-    res.render("list",{listName:day, iteamArray:items, year:fullyear});
 });
 
 app.get("/workList",function(req,res){
-    // 
-    // console.log();
     res.render("list",{listName:"Work" , iteamArray:workItems, year:fullyear});
 });
 
 app.post("/",function(req, res){
     console.log(req.body.remove);
-    var iteam=req.body.newItm;
+    var Added=req.body.newItm;
+// making new model obj for newly added iteam:
+    const newAdded=new Iteam({
+        name:Added
+    });
+    newAdded.save();
+    res.redirect("/");
 
-    if(req.body.remove==="Work" || req.body.remove===day){
-        if(req.body.remove==="Work"){
-            workItems.pop();
-            res.redirect("/workList");
-        }
-        else{
-            items.pop();
+// these are for work todolist i will see it later
+    // if(req.body.remove==="Work" || req.body.remove===day){
+    //     if(req.body.remove==="Work"){
+    //         workItems.pop();
+    //         res.redirect("/workList");
+    //     }
+    //     else{
+    //         items.pop();
+    //         res.redirect("/");
+    //     }
+    // }
+
+    // else if(req.body.button==="Work"){
+    //     workItems.push(iteam);
+    //     res.redirect("/workList");
+    // }
+    // else{
+    //     items.push(iteam);
+    //     res.redirect("/");
+    // }
+});
+
+app.post("/delete",function(req,res){
+    const removeId=req.body.remove;
+    Iteam.findByIdAndRemove(removeId,function(err){
+        if(!err){
+            console.log("successfully removed the id");
             res.redirect("/");
         }
-    }
-
-    else if(req.body.button==="Work"){
-        workItems.push(iteam);
-        res.redirect("/workList");
-    }
-    else{
-        items.push(iteam);
-        res.redirect("/");
-    }
+    });
 });
 
 app.listen(2000,function(){
