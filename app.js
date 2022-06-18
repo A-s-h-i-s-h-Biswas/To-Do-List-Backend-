2f1c0c9c23ba430ef02a16733f8ab2070ab3e08d
Ashish Biswas <ashishbiswasg@gmail.com>
2022-06-18 05:40:03 +0530

    This is version 3.0 it is capable of handaling any no. of template pages for toDoList. i will implement it more to give it a user login fasility.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const app=express();
 
+const _=require("lodash");
+
 //Mongoose packege:
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://admin-ashishbiswas__:adminAtlasashish1%40@cluster0.yqcdngn.mongodb.net/todolistDB");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -16,10 +18,21 @@ app.set("view engine","ejs");
 
 // in v-2.0 we will use Database MongoDB and Mongoose to store data
 
+//schema for home root:
 const iteamSchema=new mongoose.Schema({
     name:String
 });
+// schema for multiple dynamic templetes todolist:
+const topicSchema=new mongoose.Schema({
+    name:String,
+    iteam:[iteamSchema]
+});
+
+//mongoose model obj for home root:
 const Iteam=mongoose.model("toDoList",iteamSchema);
+//mongoose model obj for home dynamic templetes:
+const dyList=mongoose.model("dyList",topicSchema);
+
 const iteam1=new Iteam({
     name:"This is iteam 1"
 });
@@ -29,6 +42,8 @@ const iteam2=new Iteam({
 const iteam3=new Iteam({
     name:"This is iteam 3"
 });
+
+const arr=[iteam1,iteam2,iteam3];
 
 // successfully model objects are created
 
@@ -51,7 +66,7 @@ app.get("/",function(req,res){
 // added into database successfully
     Iteam.find({},function(err,foundIteam){
         if(foundIteam.length===0){
-            Iteam.insertMany([iteam1,iteam2,iteam3], function(err){
+            Iteam.insertMany(arr, function(err){
                 if(err){
                     console.log(err);
                 }
@@ -69,50 +84,71 @@ app.get("/",function(req,res){
     
 });
 
-app.get("/workList",function(req,res){
-    res.render("list",{listName:"Work" , iteamArray:workItems, year:fullyear});
+app.get("/:topic",function(req,res){
+    const topicName=_.capitalize(req.params.topic);
+    dyList.findOne({name:topicName},function(err,topicListFound){
+        if(!err){
+            if(!topicListFound){
+                const newList=new dyList({
+                    name:topicName,
+                    iteam: arr
+                });
+                newList.save();
+                res.redirect("/"+topicName);
+            }
+            else{
+                res.render("list",{listName:topicName, iteamArray:topicListFound.iteam, year:fullyear});
+            }
+        }
+    });
 });
 
 app.post("/",function(req, res){
-    console.log(req.body.remove);
-    var Added=req.body.newItm;
+    const Added=req.body.newItm;
+    const _lName=req.body.button;
 // making new model obj for newly added iteam:
     const newAdded=new Iteam({
-        name:Added
+    name:Added
     });
-    newAdded.save();
-    res.redirect("/");
+    if(_lName===day){
+        newAdded.save();
+        res.redirect("/");
+    }
+    else{
+        dyList.findOne({name:_lName},function(err,foundList){
+            if(!err){
+                foundList.iteam.push(newAdded);
+                foundList.save();
+                // console.log(foundList.iteam.push(newAdded));
+                res.redirect("/"+_lName);
+            }
+        });
+    }
 
-// these are for work todolist i will see it later
-    // if(req.body.remove==="Work" || req.body.remove===day){
-    //     if(req.body.remove==="Work"){
-    //         workItems.pop();
-    //         res.redirect("/workList");
-    //     }
-    //     else{
-    //         items.pop();
-    //         res.redirect("/");
-    //     }
-    // }
-
-    // else if(req.body.button==="Work"){
-    //     workItems.push(iteam);
-    //     res.redirect("/workList");
-    // }
-    // else{
-    //     items.push(iteam);
-    //     res.redirect("/");
-    // }
+    
 });
 
 app.post("/delete",function(req,res){
     const removeId=req.body.remove;
-    Iteam.findByIdAndRemove(removeId,function(err){
-        if(!err){
-            console.log("successfully removed the id");
-            res.redirect("/");
-        }
-    });
+    const removeTitle=req.body.listTitle;
+    // console.log(removeTitle);
+    //  console.log(removeId);
+    if(removeTitle===day){
+        Iteam.findByIdAndRemove(removeId,function(err){
+            if(!err){
+                console.log("successfully removed the id");
+                res.redirect("/");
+            }
+        });
+    }
+    else{
+        dyList.findOneAndUpdate({name: removeTitle}, {$pull: {iteam: {id: removeId}}}, function(err, foundList){
+            if (!err){
+              res.redirect("/" + removeTitle);
+            }
+          });
+    }
+    
 });
 
 app.listen(2000,function(){
